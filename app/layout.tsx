4833a4a906e825f5483aacaed3eddef708bfdd27
Eugene Kaushnian <kefbox@gmail.com>
2024-03-15 12:41:06 -0400

    Main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Header from './ui/header';
+import Footer from './ui/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,8 +13,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="min-h-full flex flex-col">
+      <body className={`${inter.className} grow flex flex-col`}>
+        <Header />
+
+        <main className="flex-1 bg-gradient-to-b from-fill-gradient-start to-fill-gradient-end to-[125%]">
+          {children}
+        </main>
+
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ app/ui/header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="bg-fill-primary">Header</header>;
+}
